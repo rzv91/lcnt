1d212891de110dfd1c388333c3bbc49f72beb71d
rzv91 <pgl_razvan@yahoo.com>
2014-09-04 19:07:14 +0300

    Finish static pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -38,6 +38,19 @@ describe "Static pages" do
     it "should have the title 'About Us'" do
       visit '/static_pages/about'
       expect(page).to have_title("About Us")
+end
+end
+
+describe "Contact" do
+
+    it "should have the content 'contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_content('contact')
+    end
+
+    it "should have the title 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_title("Contact")
     end
   end
 end
